f2c38bb5b779940a33ae55dc2cc17ca987833027
oncleben31 <oncleben31@users.noreply.github.com>
2019-06-10 09:49:57 +0200

    Add abacus method

--- src/pypool_pump/__init__.py
+++ src/pypool_pump/__init__.py
@@ -13,22 +13,26 @@ class FilteringDuration(object):
 
     @property
     def duration(self):
+        """Filtering duration in hours"""
         return self._filtering_duration
 
     @property
     def pool_mean_temperature(self):
+        """Pool temerature in °C"""
         return self._pool_mean_temperature
 
     @pool_mean_temperature.setter
     def pool_mean_temperature(self, temperature):
+        """Setter for pool temperature in °C"""
         self._pool_mean_temperature = temperature
 
 
 class DumbFilteringDuration(FilteringDuration):
-    """Dumb calulation method with taking temperaure/2"""
+    """Dumb duration calulation method with taking temperature/2"""
 
     @property
     def duration(self):
+        """Filtering duration in hours"""
         filtering_duration = self._pool_mean_temperature / 2
         if filtering_duration < 0:
             return 0
@@ -39,23 +43,55 @@ class DumbFilteringDuration(FilteringDuration):
 
 
 class BasicFilteringDuration(FilteringDuration):
-    """Basic calculation method with temperature/2 and specificities
-    below 10°C and above 30°C
+    """Basic duration calculation method with:
+    - 0 below 10°C
+    - temperature/3 between 10°C and 14°C
+    - temperature/2 between 14°C and 30°C
+    - continuous filtration above 30°C
     """
 
     @property
     def duration(self):
-        filtering_duration = self._pool_mean_temperature / 2
+        """Filtering duration in hours"""
         if self._pool_mean_temperature < 10:
             # No need to filter below 10°C.
             return 0
+        elif self._pool_mean_temperature < 14:
+            # between 10 and 14 we can reduce filtering
+            return self._pool_mean_temperature / 3
         elif self._pool_mean_temperature >= 30:
             # Above 30°C it is recommanded to filter continuously.
             return 24
         else:
-            return filtering_duration
+            return self._pool_mean_temperature / 2
 
 
-# TODO: courbe
+class AbacusFilteringDuration(FilteringDuration):
+    """Advanced calculation method using an abacus.
+    D = a*T^3 + b*T^2 + c*T +d
+    T is forced at a 10°C minimum
+    
+    
+    Formula discovered here: https://github.com/scadinot/pool
+    """
+
+    @property
+    def duration(self):
+        """Filtering duration in hours"""
+        # Force temperature at a 10°C minimum to ensure minimum filtration.
+        temperature = max(self._pool_mean_temperature, 10)
+
+        filtering_duration = (
+            0.00335 * temperature ** 3
+            - 0.14953 * temperature ** 2
+            + 2.43489 * temperature
+            - 10.72859
+        )
+        if filtering_duration > 24:
+            return 24
+
+        return filtering_duration
+
+
 # TODO: caractéristique pompe.
 # TODO: ajouter modificateur nombre de personne.
---
+++ tests/test_abacus_filtering_duration.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+"""tests for pypool_pump module - AbacusFilteringDuration"""
+import pytest
+
+from pypool_pump import AbacusFilteringDuration
+
+
+@pytest.mark.parametrize(
+    "temperature, duration",
+    [
+        (-10, 2),
+        (0, 2),
+        (9, 2),
+        (10, 2),
+        (12, 2.7),
+        (14, 3.2),
+        (20, 5),
+        (25, 9),
+        (29, 15.8),
+        (30, 18.2),
+        (50, 24),
+    ],
+)
+def test_abacus_duration(temperature, duration):
+    """Test duration calculation."""
+    pool_controler = AbacusFilteringDuration(temperature)
+    # assert pool_controler.duration == duration
+    assert abs(pool_controler.duration - duration) < 0.1
